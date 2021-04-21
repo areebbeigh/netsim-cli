89782a874ebfb73114e227353e70fd508e2ce5d7
areebbeigh <areebbeigh@gmail.com>
2021-04-21 23:35:34 +0530

    app: Add cli skeleton

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,33 @@
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+
 import Engine, { DeviceType } from './core/engine';
 
 const engine = new Engine();
 
-engine.logger.listen((log) => console.log(log.toString()));
-// engine.logger.listen((log) => console.table(log, ['type', 'host']));
+function cli() {
+  console.log(chalk`Welcome to netsim-cli! 
+Type 'help' to get started.
 
-// const [hub, hubId] = engine.addDevice('Hub1', DeviceType.HUB, 5);
-const [h1, h1Id] = engine.addDevice('Host1', DeviceType.HOST, 1);
-const [h2, h2Id] = engine.addDevice('Host2', DeviceType.HOST, 1);
-// const [h3, h3Id] = engine.addDevice('Host3', DeviceType.HOST, 1);
+Github: {blue https://github.com/areebbeigh/netsim-cli/}`);
 
-// console.log('devices:', engine.listDevices());
-// engine.connectById(hubId, 'eth0', h1Id, 'eth0');
-// engine.connectById(hubId, 'eth1', h2Id, 'eth0');
-// engine.connectById(hubId, 'eth2', h3Id, 'eth0');
-engine.connectById(h1Id, 'eth0', h2Id, 'eth0')
+  const prompt = () => {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'line',
+          message: chalk`{bold nestim-cli>}`,
+          prefix: '',
+        },
+      ])
+      .then(({ line }) => {
+        console.log(line.split(' '));
+        prompt();
+      });
+  };
 
-engine.assignIp(h1Id, 'eth0', '10.0.0.2');
-engine.assignIp(h2Id, 'eth0', '10.0.0.3');
-// engine.assignIp(h3Id, 'eth0', '10.0.0.4');
+  prompt();
+}
 
-engine.send(h1Id, '10.0.0.3', 'test');
+cli();
---
+++ src/test.ts
@@ -0,0 +1,23 @@
+import Engine, { DeviceType } from './core/engine';
+
+const engine = new Engine();
+
+engine.logger.listen((log) => console.log(log.toString()));
+// engine.logger.listen((log) => console.table(log, ['type', 'host']));
+
+// const [hub, hubId] = engine.addDevice('Hub1', DeviceType.HUB, 5);
+const [h1, h1Id] = engine.addDevice('Host1', DeviceType.HOST, 1);
+const [h2, h2Id] = engine.addDevice('Host2', DeviceType.HOST, 1);
+// const [h3, h3Id] = engine.addDevice('Host3', DeviceType.HOST, 1);
+
+// console.log('devices:', engine.listDevices());
+// engine.connectById(hubId, 'eth0', h1Id, 'eth0');
+// engine.connectById(hubId, 'eth1', h2Id, 'eth0');
+// engine.connectById(hubId, 'eth2', h3Id, 'eth0');
+engine.connectById(h1Id, 'eth0', h2Id, 'eth0')
+
+engine.assignIp(h1Id, 'eth0', '10.0.0.2');
+engine.assignIp(h2Id, 'eth0', '10.0.0.3');
+// engine.assignIp(h3Id, 'eth0', '10.0.0.4');
+
+engine.send(h1Id, '10.0.0.3', 'test');
